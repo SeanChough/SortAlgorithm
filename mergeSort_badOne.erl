2839ce738fb8c3414a42ef5cf94f231515b88375
Sean <zxxz1830248@163.com>
2018-12-09 16:03:37 +0800

    Add merge sort, also a bad program of mergeSort.
    The bad one was write some days before the mergeSort.erl. It failed to complete the conqure. But it's just a small logic fault.

---
+++ mergeSort_badOne.erl
@@ -0,0 +1,63 @@
+-module(mergeSort_badOne).
+-export([start/0]).
+%% 归并排序
+%% 将?RANGE个随机数排序：小->大
+%% This is a bad program.
+-define(RANGE,4).
+
+start() ->
+	List = genList(?RANGE,0,[]),
+	io:format("The list is:~n~w~n",[List]),
+	
+	Result = mergeSort(List),
+	io:format("The result is:~n~w~n", [Result]).
+	
+mergeSort(List) ->
+	Length = length(List),
+	splitL(1,trunc(Length/2),Length,List).
+	
+%% 分开	
+splitL(_Left,Mid,Right,List) when Right == 2 ->	%% like List == [4,9]
+	{LeftL,RightL} = lists:split(Mid,List),
+	io:format("LeftL:~p,RightL:~p~n",[LeftL,RightL]),
+	mergeL(1,LeftL,1,RightL,[]);		%% Here we shouldn't call the mergeL() function any more
+splitL(Left,Mid,_Right,List) ->				%%[Left...Mid][Mid+1...Right]
+	{LeftL,RightL} = lists:split(Mid,List),
+	LeftResult = splitL(Left,trunc(length(LeftL)/2),Mid,LeftL),
+	RightResult = splitL(1,trunc(length(RightL)/2),length(RightL),RightL),
+	Sorted = mergeL(1,LeftResult,1,RightResult,[]),
+	io:format("Sorted is:~p~n",[Sorted]),
+	Sorted.
+	
+%% 合并
+mergeL(ARef,ListA,BRef,ListB,Results) when ARef > length(ListA),BRef > length(ListB) ->
+	io:format("Results is:~p~n",[Results]),
+	Results;
+mergeL(ARef,ListA,BRef,ListB,Results) when ARef > length(ListA),BRef =< length(ListB) ->
+	NewResults = Results ++ lists:nthtail(BRef-1,ListB),
+	io:format("New results is:~p~n",[NewResults]),
+	NewResults;
+mergeL(ARef,ListA,BRef,ListB,Results) when BRef > length(ListB),ARef =< length(ListA) ->
+	NewResults = Results ++ lists:nthtail(ARef-1,ListA),
+	io:format("New results is:~p~n",[NewResults]),
+	NewResults;
+mergeL(ARef,ListA,BRef,ListB,Results) ->
+	AV = lists:nth(ARef,ListA),
+	BV = lists:nth(BRef,ListB),
+	io:format("AV:~p,BV:~p~n",[AV,BV]),
+	timer:sleep(5000),
+	if AV > BV ->
+		mergeL(ARef,ListA,BRef+1,ListB,Results ++ [BV]);
+	true ->
+		mergeL(ARef+1,ListA,BRef,ListB,Results ++ [AV])
+	end.
+			
+%% 生成随机数列
+genList(Limit,Num,List) ->
+	case Num of
+		Limit ->
+			List;
+		_ ->
+			NewList = List ++ [rand:uniform(Limit)],
+			genList(Limit, Num+1, NewList)
+	end.
